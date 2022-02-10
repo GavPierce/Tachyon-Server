121f887186873a25f7df9671e4cef093859ba4dc
EC2 Default User <ec2-user@ip-172-31-3-5.us-east-2.compute.internal>
2022-02-10 13:16:08 +0000

    backupServer file and backups

--- backupServer.sh
+++ backupServer.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-dir=/home/ec2-user/Tachyon-Server
-if [ -d "$dir" ]; then
-	    branch=$(git --git-dir "$dir/.git" branch | sed -n -e 's/^\* \(.*\)/\1/p')
-	    status=$(git --git-dir "$dir/.git" --work-tree=$dir status)
-	    git --git-dir="$dir/.git" add *
-	    git --git-dir="$dir/.git" commit -m 'Backing up Server...'
-	    git --git-dir="$dir/.git" push
-	else
-		    branch='.git dir not found'
-		        status=''
-fi
-
-echo
-echo "* Folder: $dir/.git"
-echo "* Branch: $branch"
-echo "* Status:"
-echo
-echo "$status"
-echo
+timestamp() {
+date +"%m-%d-%y"
+}
+screen -S tachyonServer -p 0 -X stuff "Doing Daily Backup...^M"
+cp -r ~/Tachyon-Server/server/DataBase/ ~/backup/
+sleep 2
+mv ~/backup/DataBase/ ~/backup/$(timestamp)
+sleep 1
+screen -S tachyonServer -p 0 -X stuff "Backup Finished!^M"
